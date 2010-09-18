26f4d4b88709555a5037af8371712a3824908d29
chmullins <chmullins@2c6a4c3f-9129-d30f-53ef-3175b4726b86>
2010-09-18 00:31:57 +0000

    Menu Updates

--- trunk/Network_Testing_Pro/Assets/TempScene/CatPicker.js
+++ trunk/Network_Testing_Pro/Assets/TempScene/CatPicker.js
@@ -1,15 +1,39 @@
 var showCatPicker = false;
+var menuSkin1 : GUISkin;
+var catPickerStyle : GUIStyle;
+
+var leftArrow : Texture2D;
+var rightArrow : Texture2D;
+
+var catFaces : Texture2D[];
 
 function OnGUI(){
 
+	GUI.skin = menuSkin1;
+	
 	var scrW = Screen.width;
 	var scrH = Screen.height;
 	if(showCatPicker)
 	{
-		if(GUI.Button(Rect(scrW/2 -50, scrH/2 -50,100,100),"PICK A CAT"))
+		GUI.BeginGroup(new Rect(scrW/2 - 150,scrH/2 -150,400,400));
+		
+		
+		if(GUI.Button(Rect(0, 50,100,100),leftArrow, "catPickerStyle"))
 		{
-			print("Cat Pick Click");
-		}	
+			print("Cat Pick Click Left");
+		}
+		
+		if(GUI.Button(Rect(300, 50,100,100),rightArrow, "catPickerStyle"))
+		{
+			print("Cat Pick Click right");
+		}
+		
+		if(GUI.Button(Rect(100, 50, 100, 100),"this cat image", "catPickerStyle"))
+		{
+			print("Cat Pick Image");
+			guiTexture.texture = catFaces[1];
+		}
+		GUI.EndGroup();
 	}
 	
 }
