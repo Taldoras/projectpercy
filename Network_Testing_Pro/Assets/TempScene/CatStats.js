d1f5e40535ec90cc8d14e2e905ace345a8957f0a
chmullins <chmullins@2c6a4c3f-9129-d30f-53ef-3175b4726b86>
2010-09-18 00:31:57 +0000

    Menu Updates

--- Network_Testing_Pro/Assets/TempScene/CatStats.js
+++ Network_Testing_Pro/Assets/TempScene/CatStats.js
@@ -7,6 +7,8 @@ function OnGUI(){
 
 	if(catStatsVisible)
 	{
+		GUI.BeginGroup (new Rect(Screen.width / 2 - 200, Screen.height / 2 - 300, 400, 600));
+		
 		GUI.Button(Rect(0,0,100,20),"Player One"); // Top Left
 		GUI.Button(Rect(scrW - 100,0,100,20),"Player One"); // Top Right
 		GUI.Button(Rect(0,scrH -20,100,20),"Player One"); // Bottom Left
