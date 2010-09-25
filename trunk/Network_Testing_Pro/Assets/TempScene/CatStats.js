801c949e7618be6b763a702be33a18572d3ae60f
chmullins <chmullins@2c6a4c3f-9129-d30f-53ef-3175b4726b86>
2010-09-25 15:10:32 +0000

    Cat picker update

--- trunk/Network_Testing_Pro/Assets/TempScene/CatStats.js
+++ trunk/Network_Testing_Pro/Assets/TempScene/CatStats.js
@@ -13,6 +13,8 @@ function OnGUI(){
 		GUI.Button(Rect(scrW - 100,0,100,20),"Player One"); // Top Right
 		GUI.Button(Rect(0,scrH -20,100,20),"Player One"); // Bottom Left
 		GUI.Button(Rect(scrW -100,scrH - 20,100,20),"Player One"); // Bottom Right
+		
+		GUI.EndGroup();
 	}
 
 }
