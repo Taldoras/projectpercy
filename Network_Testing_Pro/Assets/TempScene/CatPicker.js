9e9ac535787054816d3148a1db1fbb741d3efc26
chmullins <chmullins@2c6a4c3f-9129-d30f-53ef-3175b4726b86>
2010-09-25 15:10:32 +0000

    Cat picker update

--- Network_Testing_Pro/Assets/TempScene/CatPicker.js
+++ Network_Testing_Pro/Assets/TempScene/CatPicker.js
@@ -5,7 +5,15 @@ var catPickerStyle : GUIStyle;
 var leftArrow : Texture2D;
 var rightArrow : Texture2D;
 
-var catFaces : Texture2D[];
+var currentCatFace = 1;
+var catFaceMax = 4;
+var catFace1 : Texture2D;
+var catFace2 : Texture2D;
+var catFace3 : Texture2D;
+var catFace4 : Texture2D;
+var currentCatFaceTexture : Texture2D;
+currentCatFaceTexture = catFace1;
+
 
 function OnGUI(){
 
@@ -17,25 +25,68 @@ function OnGUI(){
 	{
 		GUI.BeginGroup(new Rect(scrW/2 - 150,scrH/2 -150,400,400));
 		
+		GUI.Label(Rect(0,0,300,100), "Choose a Character");
 		
 		if(GUI.Button(Rect(0, 50,100,100),leftArrow, "catPickerStyle"))
 		{
-			print("Cat Pick Click Left");
+			currentCatFace = currentCatFace - 1;
+			currentCatFace = showCatFace(currentCatFace);			
+			Debug.Log("Cat Pick Click Left " + currentCatFace);
 		}
 		
 		if(GUI.Button(Rect(300, 50,100,100),rightArrow, "catPickerStyle"))
 		{
-			print("Cat Pick Click right");
+			currentCatFace = currentCatFace + 1;
+			currentCatFace = showCatFace(currentCatFace);	
+			Debug.Log("Cat Pick Click right " + currentCatFace);
 		}
 		
-		if(GUI.Button(Rect(100, 50, 100, 100),"this cat image", "catPickerStyle"))
+		if(GUI.Button(Rect(100, 50, 100, 100),currentCatFaceTexture, "catPickerStyle"))
 		{
-			print("Cat Pick Image");
-			guiTexture.texture = catFaces[1];
+			Debug.Log("Cat Pick Image");
+			
 		}
+		
+		GUI.Label(Rect(0,150,300,100), "Waiting for Players");
+		
 		GUI.EndGroup();
 	}
 	
+}
+
+function showCatFace(currentCatFace)
+{
+	Debug.Log("doing showCatFace. currentCatFace: " + currentCatFace);
+	
+	if(currentCatFace <= 1)
+	{
+		currentCatFace = catFaceMax;		
+	}
+	
+	if(currentCatFace > 4)
+	{
+		currentCatFace = 1;		
+	}
+	
+	if(currentCatFace ==1)
+	{
+		currentCatFaceTexture = catFace1;
+	}
+	if(currentCatFace ==2)
+	{
+		currentCatFaceTexture = catFace2;
+	}
+	if(currentCatFace ==3)
+	{
+	currentCatFaceTexture = catFace3;
+	}
+	if(currentCatFace ==4)
+	{
+		currentCatFaceTexture = catFace4;
+	}
+
+	Debug.Log("after showCatFace. currentCatFace: " + currentCatFace);
+	return currentCatFace;
 }
 
 
