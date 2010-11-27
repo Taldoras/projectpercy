c455ef7663f9db16fc627f05c6ce1fc372f5e788
chmullins <chmullins@2c6a4c3f-9129-d30f-53ef-3175b4726b86>
2010-11-27 15:50:45 +0000

    Opacity on score menu

--- Network_Testing_Pro/Assets/EscMenu.js
+++ Network_Testing_Pro/Assets/EscMenu.js
@@ -1,12 +1,9 @@
 // Global menu for base level features
-
-
 var quitMenu = false;
-
+var audioMenu = false;
 
 function Update()
 {
-	
 	// Toggle escape menu
 	if (Input.GetKeyDown ("escape")) 
 	{ 		
@@ -18,8 +15,6 @@ function Update()
 
 function OnGUI () 
 {
-	
-
 	if (quitMenu == true)
 	{
 		// render the escape menu
@@ -31,10 +26,35 @@ function OnGUI ()
 			Application.LoadLevel("StartupScene");
 		}
 		
-		if (GUI.Button (Rect(Screen.width/2 - 75 +30, Screen.height/2 - 200 + 60,100,20),"Exit Menu"))	
+		if (GUI.Button (Rect(Screen.width/2 - 75 +30, Screen.height/2 - 200 + 60,100,20),"Audio"))	
+		{
+			audioMenu = true;
+			quitMenu = false;
+		}
+		
+		if (GUI.Button (Rect(Screen.width/2 - 75 +30, Screen.height/2 - 200 + 90,100,20),"Exit Menu"))	
 		{
 			quitMenu = false;
 		}
 		
 	}
+	
+	if (audioMenu == true)
+	{
+		// render the escape menu
+		GUI.Box (Rect (Screen.width/2 - 150,Screen.height/2 - 200,300,300), "Audio Settings");
+		
+		if (GUI.Button (Rect(Screen.width/2 - 75 +30, Screen.height/2 - 200 + 90,100,20),"Accept"))	
+		{
+			audioMenu = false;
+			quitMenu = true;
+		}
+		
+	}	
+	
+	
+	
+	
+	
+	
 }
