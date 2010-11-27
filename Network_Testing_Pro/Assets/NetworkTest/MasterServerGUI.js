f1d5dbfd73ba37282a3f442bf6c867442479aa6b
biggs.michael <biggs.michael@gmail.com>
2010-11-27 15:31:17 +0000

    removed menu, added automatic connection to already running server or automatic start of server

--- Network_Testing_Pro/Assets/NetworkTest/MasterServerGUI.js
+++ Network_Testing_Pro/Assets/NetworkTest/MasterServerGUI.js
@@ -3,6 +3,7 @@
 var gState : GlobalState;
 gState = GameObject.Find("GlobalState").GetComponent(GlobalState);
 
+var connected = false;
 
 var gameName = "Sumo Cats";
 
@@ -47,23 +48,22 @@ function OnFailedToConnect(info: NetworkConnectionError)
 	Debug.Log(info);
 }
 
-function OnGUI ()
+function Awake()
 {
-	var menuName = "Welcome " + gState.playerName;
-	windowRect = GUILayout.Window (0, windowRect, MakeWindow, menuName);
-}
+	MasterServer.ClearHostList();
+	MasterServer.RequestHostList(gameName);
 
-function Awake ()
-{
-	// Start connection test
-	natCapable = Network.TestConnection();
-	
-	// What kind of IP does this machine have? TestConnection also indicates this in the
-	// test results
-	if (Network.HavePublicAddress())
-		Debug.Log("This machine has a public IP address");
-	else
-		Debug.Log("This machine has a private IP address");
+	//natCapable = Network.TestConnection();
+
+	/*
+	var data : HostData[] = MasterServer.PollHostList();
+	if ( data.length <= 0 )
+	{
+		Debug.Log("MasterServerGUI Awake() starting server ");
+		Network.InitializeServer(32, listenPort);
+		MasterServer.RegisterHost(gameName, "Tons of fun!", "Knock the fat bastard off!");
+	}
+	*/
 }
 
 function Start()
@@ -71,9 +71,61 @@ function Start()
 	spawnTracker = GameObject.Find("SpawnPoint").GetComponent(SpawnTracker);
 }
 
-function Update() {
-	// If test is undetermined, keep running
-	if (!doneTesting) {
+var gotStartTime = false;
+var startTime;
+var startedServer = false;
+
+function Update()
+{
+	if (doneTesting)
+	{
+		if (!connected)
+		{
+			MasterServer.RequestHostList(gameName);
+			var data : HostData[] = MasterServer.PollHostList();
+			Debug.Log("MasterServerGUI Update() data length = " + data.length);
+			if ( data.length > 0 && !Network.isServer)
+			{
+				//Debug.Log("MasterServerGUI Update() connecting to server: data[0].ip: " + data[0].ip + " data[0].port: " + data[0].port);
+				// How can I tell that if the IP address is from a private network?
+				Network.useNat = false;
+				Network.Connect(data[0].ip, data[0].port);
+				connected = true;
+			}
+			else
+			{
+				Debug.Log("MasterServerGUI Update() no server to connect to ");
+				if (!gotStartTime)
+				{
+					startTime = System.DateTime.Now;
+					gotStartTime = true;
+				}
+				else
+				{
+					var currentTime = System.DateTime.Now;
+					var timeDiff = currentTime.Subtract(startTime);
+					
+					if ( timeDiff.TotalSeconds > 3 )
+					{
+						Debug.Log("MasterServerGUI Update() starting server ");
+						Network.InitializeServer(32, listenPort);
+						MasterServer.RegisterHost(gameName, "Tons of fun!", "Knock the fat bastard off!");
+						connected = true;
+					}
+				}
+			}
+		}
+		else
+		{
+			if ( Network.peerType != NetworkPeerType.Disconnected && !spawnTracker.isLocalInitialized() )
+			{
+				var playerInstance : PlayerInfo  = spawnTracker.InitServerPlayer();
+				spawnTracker.SpawnServerPlayer(playerInstance.transformViewID, playerInstance.animationViewID);
+			}
+		}
+	}
+	else
+	{
 		TestConnection();
 	}
 }
@@ -143,155 +195,10 @@ function TestConnection() {
 		default: 
 			testMessage = "Error in test routine, got " + natCapable;
 	}
-	//Debug.Log(natCapable + " " + probingPublicIP + " " + doneTesting);
+	Debug.Log(natCapable + " " + probingPublicIP + " " + doneTesting + " " + testMessage);
 }
 
 function MakeWindow (id : int) 
 {
-	var info: NetworkConnectionError;
-	
-	hideTest = GUILayout.Toggle(hideTest, "Hide test info");
-	directConnect = GUILayout.Toggle(directConnect, "Direct Connect");
-	runDedicated = GUILayout.Toggle(runDedicated, "Run as dedicated");
-		
-	if (!hideTest) {
-		GUILayout.Label(testMessage);
-		if (GUILayout.Button ("Retest connection"))
-		{
-			Debug.Log("Redoing connection test");
-			probingPublicIP = false;
-			doneTesting = false;
-			natCapable = Network.TestConnection(true);
-		}
-	}
 
-	remoteIP = GUILayout.TextField(remoteIP, 25);
-	strListenPort = GUILayout.TextField(strListenPort,10);
-
-	if (Network.peerType == NetworkPeerType.Disconnected)
-	{
-		//Debug.Log("disconn type is "+NetworkPeerType.Disconnected);
-		GUILayout.BeginHorizontal();
-
-		if(runDedicated)
-		{
-			MasterServer.dedicatedServer = true;
-		}
-		else
-		{
-			MasterServer.dedicatedServer = false;		
-		}
-		
-		// Start a new server
-		if (GUILayout.Button ("Start Server"))
-		{
-			Network.InitializeServer(32, listenPort);
-			MasterServer.RegisterHost(gameName, "Tons of fun!", "Knock the fat cat off!");
-		}		
-		
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-		// Refresh hosts
-		if (GUILayout.Button ("Refresh available Servers") || Time.realtimeSinceStartup > lastHostListRequest + hostListRefreshTimeout)
-		{
-			MasterServer.RequestHostList (gameName);
-			lastHostListRequest = Time.realtimeSinceStartup;
-		}
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-
-		GUILayout.Space(5);
-
-		if(!directConnect)
-		{
-			var data : HostData[] = MasterServer.PollHostList();
-			for (var element in data)
-			{
-				GUILayout.BeginHorizontal();
-				//Debug.Log("Hosts found.");
-				// Do not display NAT enabled games if we cannot do NAT punchthrough
-				if ( !(filterNATHosts && element.useNat) )
-				{
-					//Debug.Log("NAT punchthrough.");
-					var name = element.gameName + " " + element.connectedPlayers + " / " + element.playerLimit;
-					GUILayout.Label(name);	
-					GUILayout.Space(5);
-					var hostInfo;
-					hostInfo = "[";
-					// Here we display all IP addresses, there can be multiple in cases where
-					// internal LAN connections are being attempted. In the GUI we could just display
-					// the first one in order not confuse the end user, but internally Unity will
-					// do a connection check on all IP addresses in the element.ip list, and connect to the
-					// first valid one.
-					for (var host in element.ip)
-					{
-						hostInfo = hostInfo + host + ":" + element.port + " ";
-					}
-					hostInfo = hostInfo + "]";
-					//GUILayout.Label("[" + element.ip + ":" + element.port + "]");	
-					GUILayout.Label(hostInfo);	
-					GUILayout.Space(5);
-					GUILayout.Label(element.comment);
-					GUILayout.Space(5);
-					GUILayout.FlexibleSpace();
-					if (GUILayout.Button("Connect"))
-					{
-						// Enable NAT functionality based on what the hosts if configured to do
-						Network.useNat = element.useNat;
-						if (Network.useNat)
-							print("Using Nat punchthrough to connect");
-						else
-							print("Connecting directly to host");
-							
-						if(!directConnect)
-							Network.Connect(element.ip, element.port);			
-						//Network.Connect("127.0.0.1", element.port);			
-					}
-				}
-				else
-				{
-					Debug.Log("No NAT");
-				}
-				GUILayout.EndHorizontal();	
-			}
-		}
-		else
-		{
-			if (GUILayout.Button("Connect"))
-			{
-				Network.useNat = false; //We know address of machine and dont need NAT
-				Debug.Log("Attempting connection to "+remoteIP+":"+strListenPort);
-				info = Network.Connect(remoteIP, parseInt(strListenPort));
-				Debug.Log("Direct connect result: "+info);
-			}				
-		}
-			
-	}
-	else
-	{
-		if (GUILayout.Button ("Disconnect"))
-		{
-			Debug.Log("Disconnect button pressed.");
-			Network.Disconnect();
-			if(Network.isServer)
-			{
-				Debug.Log("I think I'm a sever!");
-				MasterServer.UnregisterHost();
-				spawnTracker.SendMessage("CleanAllPlayers");
-			}
-		}
-		if(!MasterServer.dedicatedServer && !spawnTracker.isLocalInitialized())
-		{
-			if(GUILayout.Button("Connect to Local Server"))
-			{
-				//	networkView.RPC("InitPlayer", player, player, transformViewID, animationViewID);
-				var playerInstance : PlayerInfo  = spawnTracker.InitServerPlayer();
-				spawnTracker.SpawnServerPlayer(playerInstance.transformViewID, playerInstance.animationViewID);
-			}
-		}		
-		GUILayout.FlexibleSpace();
-	}
-	GUI.DragWindow (Rect (0,0,1000,1000));
 }
